340c2231a22deaaeff221b2aa337f797d1b4d90d
Isaac9706 <isaacjosue97@outlook.com>
2021-01-21 08:26:13 -0600

    Tailwind funcionando

--- src/index.js
+++ src/index.js
@@ -1,7 +1,2 @@
 /* eslint-disable prettier/prettier */
-/* eslint-disable indent */
-/* eslint-disable eol-last */
-/* eslint-disable no-alert */
-/* eslint-disable no-console */
-// Script de Frontend
-// Importar los estilos
+import './styles/styles.css';
